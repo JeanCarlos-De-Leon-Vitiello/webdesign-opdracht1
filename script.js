1394ff5ebc4b5cb159d1c7a8e33a9e2409b8a990
Jean-Carlos2000 <jeandeleon2000@gmail.com>
2026-02-05 10:20:31 +0100

    e

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 // zorgt ervoor dat de kaart draait door een class toetevoegen/verwijderen bij een klik
-const kaart = document.querySelector(".kaart")
+const kaarten = document.querySelectorAll(".kaart")
 
-kaart.addEventListener("click", function () {
-    kaart.classList.toggle("kaartGedraaid")
-})
-
-const kaartVerplaatsen = document.querySelector("ol li:nth-of-type(1)")
-
-kaartVerplaatsen.addEventListener("click", function () {
-    kaartVerplaatsen.classList.toggle("kaartVerplaatst")
+kaarten.forEach(function (kaart) {
+    kaart.addEventListener("click", function () {
+        kaart.classList.add("kaartGedraaid")
+    })
 })
